e8c1f5dd88686cd4d4df8d8e5ddc9feb73bc1dcb
commandiron <commandiron@gmail.com>
2023-06-19 18:52:20 +0300

    .

--- lib/domain/model/job_id.dart
+++ lib/domain/model/job_id.dart
@@ -1,3 +1,13 @@
 enum JobCategory {
   shoring, excavation, breaker;
 }
+
+
+//Bu kategoriler yetmedi çünkü alt kategorileri de var bu fiyatların, mesela shoring shutcrete veya fore kazık fiyatı gelebilir.
+
+// *** Bu gelen kategorilere göre quantity kalkülasyonu yapılmalı
+// *** Ui veya state tarafından ikisinden biri seçilebilmelidir.
+
+// Aslında iç içe enum yapabilsem bunu rahatlıkla başarabilirim fakat yapamadım. Çünkü;
+// Eğer mesela shoringBoredPile veya shoringShutcrete yaparsam bu sefer bu ikisi arasında ui veya state arasından seçim yapma sıkıntım doğuyor.
+// Sistem ikisinide otomatik hesaplıyor, ben hem ikisinden birini seçebilmeyi hem de kalkülasyonu yapmak istiyorum (Sadece seçtiğimiz kalkülasyonu yapılsa çok daha iyi olur)
