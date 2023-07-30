5cbd897e9dee59fb85171eb3b8ba749e31982be9
commandiron <commandiron@gmail.com>
2023-07-30 18:17:19 +0300

    .

--- lib/presentation/cost_table/widget/delete_button.dart
+++ lib/presentation/cost_table/widget/delete_button.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kost/config/app_space.dart';
+import 'package:kost/config/app_text_style.dart';
+import 'package:kost/domain/model/category/category.dart';
 
 import '../../../domain/bloc/cost_table_bloc.dart';
 import '../../../domain/bloc/cost_table_event.dart';
@@ -16,7 +19,15 @@ class DeleteButton extends StatelessWidget {
           context: context,
           builder: (BuildContext context) {
             return AlertDialog(
-              content: const Text("Silmek istediğinize emin misiniz?"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(costCategory.jobCategory.nameTr, style: AppTextStyle.b1!.copyWith(color: Theme.of(context).colorScheme.primary),),
+                  AppSpace.vS!,
+                  Text("pozunu silmek istediğinize emin misiniz?", style: AppTextStyle.b1!,),
+                ],
+              ),
               actions: [
                 ElevatedButton(onPressed: () => Navigator.of(context).pop(false), child: const Text("Hayır")),
                 ElevatedButton(onPressed: () => Navigator.of(context).pop(true), child: const Text("Evet")),
