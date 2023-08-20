bddae620c54a917d2d8954596142d0333339f478
Emir Demirli <emirdemirli@gmail.com>
2023-08-20 15:04:05 +0300

    .

--- lib/quantity_details/presentation/widget/floor_viewer.dart
+++ lib/quantity_details/presentation/widget/floor_viewer.dart
@@ -3,7 +3,7 @@ import '../../../common/config/app_space.dart';
 import '../../../common/config/app_text_style.dart';
 import '../../domain/model/calculator/floor.dart';
 
-class FloorViewer extends StatefulWidget {
+class FloorViewer extends StatelessWidget {
   const FloorViewer({
     Key? key,
     required this.width,
@@ -28,30 +28,25 @@ class FloorViewer extends StatefulWidget {
   final void Function(String floorAreaText, int no) onFloorAreaChanged;
 
   @override
-  State<FloorViewer> createState() => _FloorViewerState();
-}
-
-class _FloorViewerState extends State<FloorViewer> {
-  @override
   Widget build(BuildContext context) {
-    final double widthPerArea = widget.width / widget.foundationArea;
-    final double floorHeight = widget.height / widget.floors.length;
+    final double widthPerArea = width / foundationArea;
+    final double floorHeight = height / floors.length;
 
     return Column(
       children: [
-        IconButton(onPressed: widget.onFloorAdd, icon: const Icon(Icons.add)),
+        IconButton(onPressed: onFloorAdd, icon: const Icon(Icons.add)),
         ListView.builder(
-          itemCount: widget.floors.length,
+          itemCount: floors.length,
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
           itemBuilder: (context, index) {
             final floorWidth =
-                _calculateFloorWidth(widthPerArea, index, widget.width);
+                _calculateFloorWidth(widthPerArea, index, width);
             return Align(
               alignment: Alignment.center,
               child: GestureDetector(
                 onTap: () {
-                  widget.onFloorClick(widget.floors[index].no);
+                  onFloorClick(floors[index].no);
                 },
                 child: Container(
                   width: floorWidth,
@@ -62,12 +57,12 @@ class _FloorViewerState extends State<FloorViewer> {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Text(
-                        widget.floors[index].floorName,
+                        floors[index].floorName,
                         style: AppTextStyle.responsiveB1(context),
                       ),
                       AppSpace.hS!,
                       Text(
-                        widget.floors[index].area.toString(),
+                        floors[index].area.toString(),
                         style: AppTextStyle.responsiveB1(context),
                       ),
                       AppSpace.hS!,
@@ -88,12 +83,12 @@ class _FloorViewerState extends State<FloorViewer> {
 
   double _calculateFloorWidth(
       double widthPerArea, int index, double totalWidth) {
-    double floorWidth = widthPerArea * widget.floors[index].area;
+    double floorWidth = widthPerArea * floors[index].area;
     if (floorWidth > totalWidth) {
       floorWidth = totalWidth;
     }
-    if (floorWidth < widget.minWidth) {
-      floorWidth = widget.minWidth;
+    if (floorWidth < minWidth) {
+      floorWidth = minWidth;
     }
     return floorWidth;
   }
