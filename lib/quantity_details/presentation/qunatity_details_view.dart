ef3eefb946730525a081758f77c4001353242f94
Emir Demirli <emirdemirli@gmail.com>
2023-08-24 16:24:08 +0300

    .

--- lib/quantity_details/presentation/qunatity_details_view.dart
+++ lib/quantity_details/presentation/qunatity_details_view.dart
@@ -37,72 +37,73 @@ class QuantityDetailsView extends StatelessWidget {
           body: CustomScrollView(
             slivers: [
               SliverToBoxAdapter(
-                  child: Column(
-                children: [
-                  AppSpace.vL!,
-                  Text(
-                    "Bina Bilgileri",
-                    style: AppTextStyle.responsiveH4B(context),
-                  ),
-                  AppSpace.vS!,
-                  Text("Detaylarını görmek istediğiniz kata dokunun",
-                      style: AppTextStyle.responsiveB2(context)
-                          .copyWith(color: Colors.grey)),
-                  AppSpace.vS!,
-                  FloorViewer(
-                    width: MediaQuery.of(context).size.width / 1.2,
-                    height: MediaQuery.of(context).size.height / 1.6,
-                    floors: state.jobCalculator.floors,
-                    onAddFloor: (int newFloorNo) {
-                      showDialog(
-                        context: context,
-                        builder: (_) {
-                          return EditFloorDialog(
-                            floor: InitialFloor(no: newFloorNo),
-                            onSubmit: (Floor? submittedFloor) {
-                              context
-                                  .read<QuantityDetailsBloc>()
-                                  .add(AddFloor(submittedFloor));
-                              Navigator.of(context).pop();
-                            },
-                          );
-                        },
-                      );
-                    },
-                    onClickFloor: (Floor floor) {
-                      showDialog(
-                        context: context,
-                        builder: (_) {
-                          return EditFloorDialog(
-                            floor: floor,
-                            onDeleteFloor: () async {
-                              final result = await showAreYouSureDialog(
-                                    context,
-                                    frontTitle: "Silmek istediğinize",
-                                  ) ??
-                                  false;
-                              if (context.mounted) {
-                                if (result) {
-                                  context
-                                      .read<QuantityDetailsBloc>()
-                                      .add(DeleteFloor(floor));
-                                  Navigator.of(context).pop();
+                child: Column(
+                  children: [
+                    AppSpace.vL!,
+                    Text(
+                      "Bina Bilgileri",
+                      style: AppTextStyle.responsiveH4B(context),
+                    ),
+                    AppSpace.vS!,
+                    Text("Detaylarını görmek istediğiniz kata dokunun",
+                        style: AppTextStyle.responsiveB2(context)
+                            .copyWith(color: Colors.grey)),
+                    AppSpace.vS!,
+                    FloorViewer(
+                      width: MediaQuery.of(context).size.width / 1.2,
+                      height: MediaQuery.of(context).size.height / 1.6,
+                      floors: state.jobCalculator.floors,
+                      onAddFloor: (int newFloorNo) {
+                        showDialog(
+                          context: context,
+                          builder: (_) {
+                            return EditFloorDialog(
+                              floor: InitialFloor(no: newFloorNo),
+                              onSubmit: (Floor? submittedFloor) {
+                                context
+                                    .read<QuantityDetailsBloc>()
+                                    .add(AddFloor(submittedFloor));
+                                Navigator.of(context).pop();
+                              },
+                            );
+                          },
+                        );
+                      },
+                      onClickFloor: (Floor floor) {
+                        showDialog(
+                          context: context,
+                          builder: (_) {
+                            return EditFloorDialog(
+                              floor: floor,
+                              onDeleteFloor: () async {
+                                final result = await showAreYouSureDialog(
+                                      context,
+                                      frontTitle: "Silmek istediğinize",
+                                    ) ??
+                                    false;
+                                if (context.mounted) {
+                                  if (result) {
+                                    context
+                                        .read<QuantityDetailsBloc>()
+                                        .add(DeleteFloor(floor));
+                                    Navigator.of(context).pop();
+                                  }
                                 }
-                              }
-                            },
-                            onSubmit: (Floor? submittedFloor) {
-                              context
-                                  .read<QuantityDetailsBloc>()
-                                  .add(EditFloor(submittedFloor));
-                              Navigator.of(context).pop();
-                            },
-                          );
-                        },
-                      );
-                    },
-                  ),
-                ],
-              )),
+                              },
+                              onSubmit: (Floor? submittedFloor) {
+                                context
+                                    .read<QuantityDetailsBloc>()
+                                    .add(EditFloor(submittedFloor));
+                                Navigator.of(context).pop();
+                              },
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         );
