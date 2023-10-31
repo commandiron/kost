c6b02eec99e37d1b9355b54fd1498ac868c4685b
commandiron <commandiron@gmail.com>
2023-10-31 14:39:39 +0300

    .

--- lib/apartment_details/domain/model/floor/floor.dart
+++ lib/apartment_details/domain/model/floor/floor.dart
@@ -1,3 +1,7 @@
+import 'package:kost/apartment_details/domain/model/floor/room/apartment_room.dart';
+import 'package:kost/apartment_details/domain/model/floor/room/common_area_room.dart';
+import 'package:kost/apartment_details/domain/model/floor/window.dart';
+
 import 'floor_section.dart';
 
 class InitialFloor extends Floor {
@@ -84,6 +88,73 @@ class Floor {
     }
     return result;
   }
+
+  //DATA
+  static List<Floor> selviliApt1Blok = [
+    Floor(
+      no: 0,
+      area: 394.36,
+      perimeter: 92.3,
+      heightWithSlab: 3.3,
+      slabHeight: 0.3,
+      isCeilingSlabHollow: true,
+      thickWallLength: 112,
+      thinWallLength: 76.5,
+      sections: [
+        CommonArea(
+          rooms: [
+            BuildingEntranceHall(area: 41.13, perimeter: 38.60)
+          ]
+        ),
+        Apartment(
+          rooms: [
+            Saloon(area: 51.93, perimeter: 30, windows: [Window(width: 7, height: 2.5)]),
+            Kitchen(area: 17, perimeter: 16.7, windows: [Window(width: 2, height: 2.5)]),
+            ApartmentHall(area: 19, perimeter: 20.70),
+            ApartmentHall(area: 4.68, perimeter: 10.20),
+            NormalRoom(area: 17.5, perimeter: 17, windows: [Window(width: 2, height: 2.5)]),
+            NormalRoom(area: 25.9, perimeter: 22.3, windows: [Window(width: 3, height: 2.5)]),
+            Bathroom(area: 5.1, perimeter: 9.40, windows: []),
+            Bathroom(area: 6.30, perimeter: 10.20, windows: []),
+            Balcony(area: 9, perimeter: 12.8),
+
+            Saloon(area: 42, perimeter: 29.60 , windows: [Window(width: 2, height: 2.5)]),
+            NormalRoom(area: 17.5, perimeter: 17, windows: [Window(width: 2, height: 2.5)]),
+            NormalRoom(area: 28.3, perimeter: 23.50, windows: [Window(width: 3, height: 2.5)]),
+            ApartmentHall(area: 4.68, perimeter: 10.20),
+            Bathroom(area: 5.1, perimeter: 9.40, windows: []),
+            Bathroom(area: 6.30, perimeter: 10.20, windows: []),
+          ]
+        )
+
+      ],
+    ),
+    Floor(
+      no: -1,
+      area: 377.40,
+      perimeter: 78.3,
+      heightWithSlab: 3.3,
+      slabHeight: 0.3,
+      isCeilingSlabHollow: true,
+      thickWallLength: 127.16,
+      thinWallLength: 73.63,
+      sections: [
+        CommonArea(
+          rooms: [
+            BuildingFloorHall(area: 36.8, perimeter: 36.5),
+            ElevatorShaft(area: 4, perimeter: 8),
+            Stairs(area: 6.45, perimeter: 10.3),
+            TechnicalArea(area: 15, perimeter: 16),
+            TechnicalArea(area: 15, perimeter: 16),
+            TechnicalArea(area: 15, perimeter: 16),
+            TechnicalArea(area: 15, perimeter: 16),
+            TechnicalArea(area: 15, perimeter: 16),
+            TechnicalArea(area: 15, perimeter: 16),
+          ]
+        )
+      ],
+    ),
+  ];
 }
 
 extension FloorExtension on Floor {
