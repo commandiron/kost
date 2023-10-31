2cbef4e0c33f3b318215d93dbc1b3b9798e84c0f
commandiron <commandiron@gmail.com>
2023-10-31 21:26:36 +0300

    .

--- lib/apartment_details/domain/model/jobs_generator/jobs_generator.dart
+++ lib/apartment_details/domain/model/jobs_generator/jobs_generator.dart
@@ -11,10 +11,13 @@ abstract class JobsGenerator {
 
   List<Job> createJobs();
 
-  double calculation(double Function() value) {
-    return value.call();
-  }
-  String explanation(String Function() value) {
-    return value.call();
-  }
+  //Aşağıdaki fonksiyonu ne için yaptığımı hatırlamıyorum, ama işe yarar
+  //bir şeydi o yüzden yorum haline getirdim.
+
+  // double calculation(double Function() value) {
+  //   return value.call();
+  // }
+  // String explanation(String Function() value) {
+  //   return value.call();
+  // }
 }
