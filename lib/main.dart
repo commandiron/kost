077da1009728ab3483ad968d77815ad0a2dcf119
commandiron <commandiron@gmail.com>
2023-10-31 21:01:22 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// MODEL EXPLANATION
-// In our project, we have two main classes: "Job" and "UnitPrice"
-// The "Job" class is created within the "quantity_details" feature package and typically represents tasks and quantities.
-// The "UnitPrice" class is created in the "data" package and is related to current prices.
-// These two classes are merged within the "Cost" class in the "cost_table" package for cost calculations and ui.
