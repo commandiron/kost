3f40d283ad85e64fc674512f9640b1e0a5085160
commandiron <commandiron@gmail.com>
2023-10-31 14:42:23 +0300

    .

--- lib/apartment_details/domain/bloc/apartment_details_bloc.dart
+++ lib/apartment_details/domain/bloc/apartment_details_bloc.dart
@@ -4,16 +4,11 @@ import 'package:kost/common/bloc/bloc_state.dart';
 import 'package:kost/cost_table/presentation/cost_table_screen.dart';
 import 'package:kost/apartment_details/domain/bloc/apartment_details_event.dart';
 import 'package:kost/apartment_details/domain/bloc/apartment_details_state.dart';
-import 'package:kost/apartment_details/domain/model/floor/floor_section.dart';
 import 'package:kost/apartment_details/domain/model/jobs_generator/impl/apartment_jobs_generator.dart';
 
 import '../model/floor/floor.dart';
-import '../model/floor/room/apartment_room.dart';
-import '../model/floor/room/common_area_room.dart';
 import '../model/project_constants.dart';
 import '../model/project_variables.dart';
-import '../model/floor/room/room.dart';
-import '../model/floor/window.dart';
 
 class ApartmentDetailsBloc
     extends Bloc<ApartmentDetailsEvent, ApartmentDetailsState> {
@@ -38,123 +33,12 @@ class ApartmentDetailsBloc
               elevationTowerArea: 27, //✓
               elevationTowerHeightWithoutSlab: 3, //✓
             ),
-            floors: [
-              ...Floor.duplicateFloors(
-                Floor(
-                  no: 1,
-                  area: 228.68,
-                  perimeter: 63.6,
-                  heightWithSlab: 3.3,
-                  slabHeight: 0.3,
-                  isCeilingSlabHollow: true,
-                  thickWallLength: 88.24,
-                  thinWallLength: 49.54,
-                  sections: [
-                    CommonArea(
-                      rooms: [
-                        ElevatorShaft(area: 9.5, perimeter: 12.6),
-                        Stairs(area: 6.86, perimeter: 10.5),
-                        Stairs(area: 14.73, perimeter: 15.5),
-                        BuildingFloorHall(area: 4.65, perimeter: 9.25,),
-                      ]
-                    ),
-                    Apartment(
-                      rooms: [
-                        ApartmentHall(area: 10.66, perimeter: 20.65),
-                        Saloon(area: 27.36, perimeter: 23.20, windows: [Window(width: 19, height: 2.5,)]),
-                        Kitchen(area: 7.58, perimeter: 11.55, windows: []),
-                        NormalRoom(area: 7.45, perimeter: 10.95, windows: []),
-                        NormalRoom(area: 7.96, perimeter: 11.85, windows: []),
-                        NormalRoom(area: 12.11, perimeter: 15.15, windows: []),
-                        Bathroom(area: 4.87, perimeter: 9.05, windows: []),
-                        EscapeHallBathroom(area: 3.08, perimeter: 7.05, windows: []),
-                        Balcony(area: 2.53, perimeter: 6.40),
-                      ]
-                    ),
-                    Apartment(
-                      rooms: [
-                        ApartmentHall(area: 10.66, perimeter: 20.65),
-                        Saloon(area: 27.36, perimeter: 23.20, windows: []),
-                        Kitchen(area: 7.58, perimeter: 11.55, windows: []),
-                        NormalRoom(area: 7.45, perimeter: 10.95, windows: []),
-                        NormalRoom(area: 7.96, perimeter: 11.85, windows: []),
-                        NormalRoom(area: 12.11, perimeter: 15.15, windows: []),
-                        Bathroom(area: 4.87, perimeter: 9.05, windows: []),
-                        EscapeHallBathroom(area: 3.08, perimeter: 7.05, windows: []),
-                        Balcony(area: 2.53, perimeter: 6.40),
-                      ]
-                    )
-                  ],
-                ),
-                count: 9,
-              ),
-              Floor(
-                no: 0,
-                area: 166.68,
-                perimeter: 59.6,
-                heightWithSlab: 3.3,
-                slabHeight: 0.3,
-                isCeilingSlabHollow: true,
-                thickWallLength: 86.2,
-                thinWallLength: 15.5,
-                sections: [
-                  CommonArea(
-                    rooms: [
-                      ElevatorShaft(area: 9.5, perimeter: 12.6),
-                      Stairs(area: 6.86, perimeter: 10.5),
-                      Stairs(area: 14.73, perimeter: 15.5),
-                      BuildingEntranceHall(area: 4.65, perimeter: 9.25),
-                    ]
-                  ),
-                  Apartment(
-                    rooms: [
-                      SaloonWithKitchen(area: 28.06, perimeter: 30.95, windows: [Window(width: 17.4, height: 2.5,)]),
-                      NormalRoom(area: 12.93, perimeter: 18.6, windows: []),
-                      Bathroom(area: 4.74, perimeter: 8.75, windows: []),
-                      EscapeHallBathroom(area: 3.37, perimeter: 7.35, windows: []),
-                    ]
-                  ),
-                  Apartment(
-                    rooms: [
-                      SaloonWithKitchen(area: 28.06, perimeter: 30.95, windows: []),
-                      NormalRoom(area: 12.93, perimeter: 18.6, windows: []),
-                      Bathroom(area: 4.74, perimeter: 8.75, windows: []),
-                      EscapeHallBathroom(area: 3.37, perimeter: 7.35, windows: []),
-                    ]
-                  )
-                ],
-              ),
-              Floor(
-                no: -1,
-                area: 660.41,
-                perimeter: 108.90,
-                heightWithSlab: 3.3,
-                slabHeight: 0.3,
-                isCeilingSlabHollow: false,
-                thickWallLength: 50,
-                thinWallLength: 0,
-                sections: [
-                  CommonArea(
-                    rooms: [
-                      ElevatorShaft(area: 9.5, perimeter: 12.6),
-                      Stairs(area: 6.86, perimeter: 10.5),
-                      Stairs(area: 14.73, perimeter: 15.5),
-                      BuildingFloorHall(area: 4.65, perimeter: 9.25, doors: [Door.fire]),
-                      FireEscapeHall(area: 3.08, perimeter: 7.05),
-                      ParkingArea(area: 500, perimeter: 120),
-                      TechnicalArea(area: 7.10, perimeter: 10.7),
-                      TechnicalArea(area: 7.25, perimeter: 10.8),
-                      TechnicalArea(area: 17, perimeter: 16.6),
-                      TechnicalArea(area: 52.6, perimeter: 33.6),
-                    ]
-                  )
-                ],
-              ),
-            ],
+            floors: const [],
           ),
         ) {
     on<InitApartmentDetailsBloc>((event, emit) {
-      emit(state.copyWith(floors: getSortedFloors(state.floors)));
+      final floors = Floor.selviliApt1Blok;
+      emit(state.copyWith(floors: getSortedFloors(floors)));
     });
     on<AddFloor>((event, emit) {
       if(event.floor == null) {
